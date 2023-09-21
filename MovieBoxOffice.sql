de3759d1f0fd50d70fcd660edcbea23ef4875664
J1briel <145366828+J1briel@users.noreply.github.com>
2023-09-21 06:25:12 -0500

    Update MovieBoxOffice.sql

--- MovieBoxOffice.sql
+++ MovieBoxOffice.sql
@@ -7,7 +7,6 @@ Create table Movies
   Length_minutes int)
 
 
-
 Table 2 Query:
 Create table BoxOffice
 ( Movie_id int,
@@ -16,7 +15,6 @@ Create table BoxOffice
   International_Sales int)
 
 
-  
 Table 1 Insert:
 Insert into table Movies VALUES
 (1, "Toy Story", "John Lasseter", 1995, 81)
@@ -38,40 +36,90 @@ Insert into table Movies VALUES
   
 Table 2 Insert:
 Insert into table BoxOffice VALUES
-(5,	8.2,	380843261,	555900000)
-(14,	7.4,	268492764,	475066843)
-(8,	8,	206445654,	417277164)
-(12,	6.4,	191452396,	368400000)
-(3,	7.9,	245852179,	239163000)
-(6,	8,	261441092,	370001000)
+(5, 8.2,	380843261,	555900000)
+(14, 7.4,	268492764,	475066843)
+(8, 8,	206445654,	417277164)
+(12, 6.4,	191452396,	368400000)
+(3, 7.9,	245852179,	239163000)
+(6, 8,	261441092,	370001000)
 (9, 8.5,	223808164,	297503696)
-(11,	8.4,	415004880,	648167031)
+(11, 8.4,	415004880,	648167031)
 (1,	8.3,	191796233,	170162503)
-(7,	7.2,	244082982,	217900167)
-(10,	8.3,	293004164,	438338580)
-(4,	8.1,	289916256,	272900000)
+(7, 7.2,	244082982,	217900167)
+(10, 8.3,	293004164,	438338580)
+(4, 8.1,	289916256,	272900000)
 (2,	7.2,	162798565,	200600000)
-(13,	7.2,	237283207,	301700000)
+(13, 7.2,	237283207,	301700000)
 
 
+-- Find the movie with a row ID of 6 
+SELECT ID, title FROM Movies 
+  WHERE ID = 6;
 
 
+--Find the movies released in the years between 2000 and 2010
+SELECT Title, year FROM Movies
+  WHERE year BETWEEN 2000 AND 2010;
 
 
+-- Find the movies NOT released in the years between 2000 and 2010 
+SELECT Title, Year FROM Movies
+  WHERE Year < 2000 OR year > 2010;
 
 
+-- Find the first 5 Pixar movies and their release year 
+SELECT Title, Year FROM Movies
+  WHERE Year <= 2003;
 
 
+-- Find all Toy Story Movies
+SELECT title, director FROM movies 
+  WHERE title LIKE "Toy Story%";
 
 
+--Find the domestic and international sales for each movie
+SELECT title, domestic_sales, international_sales 
+FROM Movies
+  JOIN BoxOffice
+    ON movies.id = boxoffice.movie_id;
 
 
+--Show the sales numbers for each movie that did better internationally rather than domestically
+SELECT title, domestic_sales, international_sales
+FROM Movies
+  JOIN BoxOffice
+    ON movies.id = boxoffice.movie_id
+WHERE International_sales > Domestic_sales;
 
 
+-- List all the movies by their ratings in descending order 
+SELECT Title, Rating
+FROM Movies
+  JOIN BoxOffice
+    ON movies.id = boxoffice.movie_id
+ORDER BY Rating DESC;
 
 
+-- List the last four Pixar movies released (ordered from most recent to least) 
+SELECT DISTINCT Director FROM Movies
+ORDER BY Director ASC;
 
 
+-- List the last four Pixar movies released (ordered from most recent to least) 
+SELECT Title, year FROM Movies
+ORDER BY year DESC
+LIMIT 4;
 
+
+-- List the first five Pixar movies sorted alphabetically 
+SELECT Title FROM Movies
+ORDER BY Title ASC
+LIMIT 5;
+
+
+-- List the next five Pixar movies sorted alphabetically 
+SELECT Title FROM Movies
+ORDER BY Title ASC
+LIMIT 5 OFFSET 5;
 
 
